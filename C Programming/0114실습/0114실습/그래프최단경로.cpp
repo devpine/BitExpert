2594b549a96836d27b4bc443876ad8d42b339e9f
cs950309 <cs950309@gmail.com>
2020-01-14 21:12:49 +0900

    update 0114

--- C Programming/0114실습/0114실습/그래프최단경로.cpp
+++ C Programming/0114실습/0114실습/그래프최단경로.cpp
@@ -1,6 +1,11 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #pragma warning(disable:4996)
+
 #define MAX_VERTICES 6
+#define TRUE 1
+#define FALSE 0
 
 int cost[][MAX_VERTICES] =
 { {0,50,10,1000,45,1000},
@@ -12,82 +17,73 @@ int cost[][MAX_VERTICES] =
 
 int distance[MAX_VERTICES];
 short int found[MAX_VERTICES];
-int n = MAX_VERTICES;
+int path[MAX_VERTICES];
+//int n = MAX_VERTICES;
 
 void shortestpath(int ,int);
 int choose(int[], int, short int[]);
-void printpath(int, int);
+void print_path(int, int);
 
 int main()
 {
 	int start, destination;
-	printf("Input start point and destination point : ");
+	printf("시작점 도착점 입력 : ");
 	scanf("%d %d", &start, &destination);
-	if (start > MAX_VERTICES || destination > MAX_VERTICES)
-		printf("Length error\n");
-	
-	else
-	{
-		printpath(start,destination);
-	}
+
+	shortestpath(start, MAX_VERTICES);
+
+	printf("거리 : %d \n", distance[destination]);
+	print_path(start, destination);
 		
 	return 0;
 }
 
-void printpath(int start, int destination)
+void print_path(int start, int destination)
 {
-	int i;
-	int result=0;
+	if (path[destination] != start)
+		print_path(start, path[destination]);
+	printf("<%d -> %d> ", path[destination], destination);
 
-	shortestpath(start, destination);
-
-	/*for (i = 0; i <= destination; i++)
-	{
-		if (found[i] == true)
-		{
-			result += distance[i];
-			printf("%d ", i);
-		}	
-	}
-	printf("%d\n", result);*/
-	printf("%d에서 %d까지의 최단거리 %d", start, destination, distance[destination]);
 }
 
-void shortestpath(int v, int destination)
+void shortestpath(int v, int n)
 {
 	int i, u, w;
-	for (i = 0; i < n +1; i++) {
-		found[i] = false;
+
+	for (i = 0; i < n ; i++) {
+		path[i] = v;
+		found[i] = FALSE;
 		distance[i] = cost[v][i];
 	}
 
-	found[v] = true;
-	distance[v] = 0;
+	found[v] = TRUE;
+	distance[v] = TRUE;
 
-	for (i = 0; i < n - 2 ; i++) {
+	for (i = 0; i < n - 1 ; i++) {
 		u = choose(distance, n, found);
 
 		found[u] = true;
 	
 
-		for (w = 0; w < n + 1 ; w++)
+		for (w = 0; w < n  ; w++)
 			if (!found[w])
-				if (distance[u] + cost[u][w] < distance[w])
+				if (distance[u] + cost[u][w] < distance[w]) {
 					distance[w] = distance[u] + cost[u][w];
+					path[w] = u;
+				}
 	}
 }
 
 int choose(int distance[], int n, short int found[]) 
 {
 	int i, min, minpos;
-	min = 9999;
+	min = INT_MAX;
 	minpos = -1;
 
-	for (i = 0; i < n + 1 ; i++) {
+	for (i = 0; i < n  ; i++) {
 		if (distance[i] < min && !found[i]) {
 			min = distance[i];
 			minpos = i;
-			printf("%d ", minpos);
 		}
 	}
 	return minpos;
